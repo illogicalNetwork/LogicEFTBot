da8cc95039db63bd0050c8d67913abb5ac8cfa76
LogicalSoIutions <67069181+LogicalSoIutions@users.noreply.github.com>
2020-08-06 22:12:31 -0700

    Updated settings.py
    It's gross, don't booli.

--- settings.py
+++ settings.py
@@ -5,15 +5,28 @@ nick = 'LogicEFTBot'
 prefix = '!'
 initial_channels = ['LogicalSolutions','WillerZ','lvndmark']
 default_cooldown = 10
-slot_link = ''
-ammo_link = ''
-price_link = ''
-wiki_link = ''
-medical_link = ''
-trader_link = ''
+default_lang = 'en'
+slot_link_en = ''
+ammo_link_en = ''
+price_link_en = ''
+wiki_link_en = ''
+medical_link_en = ''
+trader_link_en  = ''
+slot_link_es = ''
+ammo_link_es = ''
+price_link_es = ''
+wiki_link_es = ''
+medical_link_es = ''
+trader_link_es  = ''
+slot_link_de = ''
+ammo_link_de = ''
+price_link_de = ''
+wiki_link_de = ''
+medical_link_de = ''
+trader_link_de  = ''
 database_config = {
     'host': 'localhost',
-    'user': '',
+    'user': 'root',
     'passwd': '',
     'database': ''
 }
