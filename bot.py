fa04bd3cc095b620cd76bf570d73cf138b5806ab
LogicalSoIutions <67069181+LogicalSoIutions@users.noreply.github.com>
2020-08-11 20:01:23 -0700

    Added Logging
    Added very basic logging for now - will improve it later

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import mysql.connector as mysql
 from datetime import datetime, timedelta
 import asyncio
 import time
+import logging
 
 #db config
 class database:
@@ -17,6 +18,7 @@ class database:
             passwd = settings.database_config["passwd"],
             database = settings.database_config["database"]
         )
+
         sql = db.cursor(buffered=True)
         db.commit()
         self.db = db
@@ -64,8 +66,6 @@ async def check_database():
             if not i in channels:
                 unjoined_channels.append(i)
                 channels.append(i)
-
-        print('Joining new channels: {}\n'.format(str(unjoined_channels)))
         otp = await bot.join_channels(unjoined_channels)
 
 def check_cooldown(name, cooldown_obj):
@@ -83,10 +83,15 @@ db = database()
 channels = list(dict.fromkeys(db.get_channels()+settings.initial_channels))
 loop = asyncio.get_event_loop()
 asyncio.ensure_future(check_database())
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('twitchBot.log', 'a'))
+print = logger.info
 
 locale = []
 with open("localizations.json", "r") as read_file:
     locale = json.load(read_file)
+
 
 bot = commands.Bot(
     loop=loop,
@@ -112,10 +117,11 @@ async def event_ready():
     
 @bot.command(name="price")
 async def price(ctx, *req):
+    #Look for cooldown object in cooldowns global variable
     cd = None
     for i in cooldowns:
         if i['name'] == ctx.channel.name:
-            cd = check_cooldown(ctx.channel.name, i)
+            cd = check_cooldown(ctx.channel.name, i) #Check if cooldown is over
             cd_obj = i
             break
 
@@ -127,6 +133,7 @@ async def price(ctx, *req):
         await ctx.channel.send('@{} - {}'.format(ctx.author.name, locale[check_lang(ctx.channel.name)]["validItem"]))
         return
 
+    #Check if cooldown object for channel is in cooldowns list and update last usage
     if cd is None:
         cooldowns.append({'name': ctx.channel.name, 'last_usage': datetime.utcnow()})
     else:
@@ -139,10 +146,14 @@ async def price(ctx, *req):
     price_link = None
     if lang == "en":
         price_link = settings.price_link_en
-    elif lang == "ru":
+    elif lang == "de":
         price_link = settings.price_link_de
     elif lang == "es":
         price_link = settings.price_link_es
+    elif lang == "tr":
+        price_link = settings.price_link_tr
+    elif lang == "nl":
+        price_link = settings.price_link_nl
     crafted_url = price_link + "{}".format(req)
     response = requests.get(url = crafted_url).text
     ###NEW
@@ -150,10 +161,11 @@ async def price(ctx, *req):
 
 @bot.command(name="slot")
 async def slot(ctx, *req):
+    #Look for cooldown object in cooldowns global variable
     cd = None
     for i in cooldowns:
         if i['name'] == ctx.channel.name:
-            cd = check_cooldown(ctx.channel.name, i) 
+            cd = check_cooldown(ctx.channel.name, i) #Check if cooldown is over
             cd_obj = i
             break
 
@@ -163,6 +175,8 @@ async def slot(ctx, *req):
     if len(req) == 0:
         await ctx.channel.send('@{} - {}'.format(ctx.author.name, locale[check_lang(ctx.channel.name)]["validItem"]))
         return
+
+    #Check if cooldown object for channel is in cooldowns list and update last usage
     if cd is None:
         cooldowns.append({'name': ctx.channel.name, 'last_usage': datetime.utcnow()})
     else:
@@ -173,22 +187,26 @@ async def slot(ctx, *req):
     lang = check_lang(ctx.channel.name)
     price_link = None
     if lang == "en":
-        price_link = settings.price_link_en
-    elif lang == "ru":
-        price_link = settings.price_link_de
+        slot_link = settings.slot_link_en
+    elif lang == "de":
+        slot_link = settings.slot_link_de
     elif lang == "es":
-        price_link = settings.price_link_es
-    crafted_url = price_link + "{}".format(req)
+        slot_link = settings.slot_link_es
+    elif lang == "tr":
+        slot_link = settings.slot_link_tr
+    elif lang == "nl":
+        slot_link = settings.slot_link_nl 
+    crafted_url = slot_link + "{}".format(req)
     response = requests.get(url = crafted_url).text
     await ctx.channel.send('@{} {}'.format(ctx.author.name, response))
 
 @bot.command(name="wiki")
 async def wiki(ctx, *req):
-
+    #Look for cooldown object in cooldowns global variable
     cd = None
     for i in cooldowns:
         if i['name'] == ctx.channel.name:
-            cd = check_cooldown(ctx.channel.name, i)
+            cd = check_cooldown(ctx.channel.name, i) #Check if cooldown is over
             cd_obj = i
             break
 
@@ -198,6 +216,8 @@ async def wiki(ctx, *req):
     if len(req) == 0:
         await ctx.channel.send('@{} - {}'.format(ctx.author.name, locale[check_lang(ctx.channel.name)]["validItem"]))
         return
+
+    #Check if cooldown object for channel is in cooldowns list and update last usage
     if cd is None:
         cooldowns.append({'name': ctx.channel.name, 'last_usage': datetime.utcnow()})
     else:
@@ -208,21 +228,26 @@ async def wiki(ctx, *req):
     lang = check_lang(ctx.channel.name)
     price_link = None
     if lang == "en":
-        price_link = settings.price_link_en
-    elif lang == "ru":
-        price_link = settings.price_link_de
+        wiki_link = settings.wiki_link_en
+    elif lang == "de":
+        wiki_link = settings.wiki_link_de
     elif lang == "es":
-        price_link = settings.price_link_es
-    crafted_url = price_link + "{}".format(req)
+        wiki_link = settings.wiki_link_es
+    elif lang == "tr":
+        wiki_link = settings.wiki_link_tr
+    elif lang == "nl":
+        wiki_link = settings.wiki_link_nl
+    crafted_url = wiki_link + "{}".format(req)
     response = requests.get(url = crafted_url).text
     await ctx.channel.send('@{} {}'.format(ctx.author.name, response))
 
 @bot.command(name="astat")
 async def astat(ctx, *req):
+    #Look for cooldown object in cooldowns global variable
     cd = None
     for i in cooldowns:
         if i['name'] == ctx.channel.name:
-            cd = check_cooldown(ctx.channel.name, i)
+            cd = check_cooldown(ctx.channel.name, i) #Check if cooldown is over
             cd_obj = i
             break
 
@@ -233,6 +258,7 @@ async def astat(ctx, *req):
         await ctx.channel.send('@{} - {}'.format(ctx.author.name, locale[check_lang(ctx.channel.name)]["validAmmo"]))
         return
 
+    #Check if cooldown object for channel is in cooldowns list and update last usage
     if cd is None:
         cooldowns.append({'name': ctx.channel.name, 'last_usage': datetime.utcnow()})
     else:
@@ -243,28 +269,37 @@ async def astat(ctx, *req):
     lang = check_lang(ctx.channel.name)
     price_link = None
     if lang == "en":
-        price_link = settings.price_link_en
-    elif lang == "ru":
-        price_link = settings.price_link_de
+        ammo_link = settings.ammo_link_en
+    elif lang == "de":
+        ammo_link = settings.ammo_link_de
     elif lang == "es":
-        price_link = settings.price_link_es
-    crafted_url = price_link + "{}".format(req)
+        ammo_link = settings.ammo_link_es
+    elif lang == "tr":
+        ammo_link = settings.ammo_link_tr
+    elif lang == "nl":
+        ammo_link = settings.ammo_link_nl  
+    crafted_url = ammo_link + "{}".format(req)
     response = requests.get(url = crafted_url).text
     await ctx.channel.send('@{} {}'.format(ctx.author.name, response))
 
 @bot.command(name="medical")
 async def medical(ctx, *req):
+    #Look for cooldown object in cooldowns global variable
     cd = None
     for i in cooldowns:
         if i['name'] == ctx.channel.name:
-            cd = check_cooldown(ctx.channel.name, i)
+            cd = check_cooldown(ctx.channel.name, i) #Check if cooldown is over
             cd_obj = i
             break
+
     if cd or ctx.author.name.lower() == settings.nick:
         return
+
     if len(req) == 0:
         await ctx.channel.send('@{} - {}'.format(ctx.author.name, locale[check_lang(ctx.channel.name)]["validMed"]))
         return
+
+    #Check if cooldown object for channel is in cooldowns list and update last usage
     if cd is None:
         cooldowns.append({'name': ctx.channel.name, 'last_usage': datetime.utcnow()})
     else:
@@ -275,12 +310,16 @@ async def medical(ctx, *req):
     lang = check_lang(ctx.channel.name)
     price_link = None
     if lang == "en":
-        price_link = settings.price_link_en
+        medical_link = settings.medical_link_en
     elif lang == "de":
-        price_link = settings.price_link_de
+        medical_link = settings.medical_link_de
     elif lang == "es":
-        price_link = settings.price_link_es
-    crafted_url = price_link + "{}".format(req)
+        medical_link = settings.medical_link_es
+    elif lang == "tr":
+        medical_link = settings.medical_link_tr
+    elif lang == "nl":
+        medical_link = settings.medical_link_nl  
+    crafted_url = medical_link + "{}".format(req)
     response = requests.get(url = crafted_url).text
     await ctx.channel.send('@{} {}'.format(ctx.author.name, response))
 
@@ -295,7 +334,7 @@ async def help(ctx, req=None):
     return
 
 @bot.command(name="addbot")
-async def addbot(ctx, req=None): #Later change this to invite link
+async def addbot(ctx, req=None):
     await ctx.channel.send('@{} - {}'.format(ctx.author.name, locale[check_lang(ctx.channel.name)]["addBot"]))
     return
 
